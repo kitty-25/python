6ad9af616e81dadbc34bfae1bc2c25442aec4e0f
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-14 23:43:00 +0930

    14 may 2021 assignment

--- atm.py
+++ atm.py
@@ -1,9 +1,27 @@
-#simulate ATM logic
+#ATM
+
+amt=20000
 pin=int(input("Enter your pin:"))
 
-if ((pin>=1000 and pin<=9999) and (pin%10==2 or pin%10==7)):
-    print(f"Your pin is {pin}")
-else:
-    print(f"Invalid pin")
-    
+if(pin == 2345):
+    print("Verification Successful")
 
+    option=int(input("Please selsct your option: 1.Withdraw 2.Balance Enquiry 3.Deposit 4.Exit"))
+    if(option==1):
+        amount=int(input("Please enter your withdrawal amount:"))
+        if(amount < amt):
+            withdrawal = amt-amount
+            print(f"Your current balance is {withdrawal}")
+        else:
+            print("You have exceeded your balance")
+    elif(option==2):
+        print(f"Your current balance is {amt}")
+    elif(option==3):
+        dep=int(input("Please enter your deposit amount:"))
+        deposit = amt+dep
+        print(f"Your current balance is {deposit}")
+    elif(option==4):
+        print("Thank you for using ATM")
+            
+else:
+    print("You have entered the wrong pin")
