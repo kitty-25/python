80817da9b76098f99efa3dbcb0ccaba26b7f9b22
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-17 23:51:26 +0930

    17 may 2021

---
+++ series.py
@@ -0,0 +1,21 @@
+#10+8+6+4+2=?
+
+n=int(input("Enter the number:"))
+sum=0
+for i in range(n,0,-2):
+    print(i,end="+")
+    sum+=i
+print(str(i-2)+"="+str(sum))
+
+
+#if n=10
+#start  stop  sum+=i  result  step
+#(n-2)
+# 10     0    0+10=10   10     10
+# 8      0    10+8=18   18      8
+# 6      0    18+6=24   24      6
+# 4      0    24+4=28   28      4
+# 2      0    28+2=30   30      2
+# stop at 0
+
+
