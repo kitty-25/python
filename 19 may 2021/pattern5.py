4ee36a4b909289253f4cd86e359b8bf6c8d3b535
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-20 08:58:41 +0930

    Add files via upload

---
+++ 19 may 2021/pattern5.py
@@ -0,0 +1,17 @@
+#5 4 3 2 1
+#5 4 3 2 1
+#5 4 3 2 1
+#5 4 3 2 1
+#5 4 3 2 1
+
+for i in range(1,6):
+    print()
+    for j in reversed(range(1,6)):
+        print(j,end=" ")
+
+#iteration      i     j (executed 5 time with thier range)
+#   0           1     5 4 3 2 1
+#   1           2     5 4 3 2 1
+#   2           3     5 4 3 2 1
+#   3           4     5 4 3 2 1
+#   4           5     5 4 3 2 1
