8d35b5a2a023e5a04a25ee668ccc2fbc1bc77ecd
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-19 14:18:06 +0930

    Add files via upload

---
+++ 14 may 2021/enquiry.py
@@ -0,0 +1,17 @@
+#course enquiry for 4 courses (python.js.java and ML)
+
+course=input("Enter the course you want to enquire:")
+if(course == "python"):
+    print("Duration: 45days")
+    print("Fees: 15K")
+elif(course == "java"):
+    print("Duration: 90days")
+    print("Fees: 12K")
+elif(course == "js"):
+    print("Duration: 45days")
+    print("Fees: 20K")
+elif(course == "ML"):
+    print("Duration: 90days")
+    print("Fees: 25K")
+else:
+    print("Sorry, The course you have enquired is unavailable")
