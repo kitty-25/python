87dba4a6ef59159b5c9a1be6e9179abc1dcba3c2
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-19 13:44:37 +0930

    Delete atmpin.py

--- atmpin.py
+++
@@ -1,9 +0,0 @@
-#simulate ATM logic
-pin=int(input("Enter your pin:"))
-
-if ((pin>=1000 and pin<=9999) and (pin%10==2 or pin%10==7)):
-    print(f"Your pin is {pin}")
-else:
-    print(f"Invalid pin")
-    
-
