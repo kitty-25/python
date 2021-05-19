3f5e9aeaba92d1ca82cfe51ac288ef2b88017909
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-19 23:34:39 +0930

    uploaded pattern2.py with iteration

---
+++ 19 may 2021/pattern2.py
@@ -0,0 +1,18 @@
+#pattern
+#11111
+#22222
+#33333
+#44444
+#55555
+
+for i in range(1,6):
+    print()
+    for j in range(5):
+        print(i,end=" ")
+        
+#iteration      i          j   
+#  1            1         11111
+#  2            2         22222
+#  3            3         33333
+#  4            4         44444
+#  5            5         55555
