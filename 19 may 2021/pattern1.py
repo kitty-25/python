31c2865bedd477cc4154e6023f8d18200be45f53
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-19 23:38:34 +0930

    uploaded pattern1.py

---
+++ 19 may 2021/pattern1.py
@@ -0,0 +1,14 @@
+#using nested loop, print * in matrix format
+
+for i in range(5):
+    print()
+    for j in range(5):
+        print("*",end=" ")
+
+#iteration    i     j
+#   1         *    * * * * *
+#   2         *    * * * * *
+#   3         *    * * * * *
+#   4         *    * * * * *
+#   5         *    * * * * *
+        
