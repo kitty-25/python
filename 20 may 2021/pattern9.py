4e405180d33075f3d2e9c7b99b27ed3d9fa43d27
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-20 23:01:59 +0930

    Create pattern9.py

---
+++ 20 may 2021/pattern9.py
@@ -0,0 +1,16 @@
+#1  2  3  4  5
+#2  4  6  8 10
+#3  6  9 12 15
+#4  8 12 16 20
+#5 10 15 20 25
+
+for i in range(1,6):
+    
+    for j in range(1,6):
+       res=i*j
+       if res>9:
+           print(str(res), end=" ")
+       else:
+            print(" "+str(res),end=" ")
+       
+    print()
