f9862fe6935778c780766e5f058de44fdcadd8b3
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-20 21:27:37 +0930

    Update strong.py

--- 18 may 2021/strong.py
+++ 18 may 2021/strong.py
@@ -2,22 +2,28 @@
 
 n=int(input("Enter the number:"))
 
-strong=0
-fact=1
+sum=0
 temp=n
 
 while n!=0:
-    strong+=n%10
+    strong=n%10
     n//=10
-    
-    for i in range(1,n):
-        fact*=i
 
-    strong+=fact
+    fact=1
+    for i in range(1,strong+1):
+        fact*=i
+    sum+=fact
     
-print(strong)
+print(sum)
         
-if(temp==strong):
-    print(f"{temp} is a strong number")
+if(temp==sum):
+    print(f"{sum} is a strong number")
 else:
-    print(f"{temp} is not a strong number")
+    print(f"{sum} is not a strong number")
+    
+
+#iteration n%10	 n//=10	  fact		sum
+#   1        5	  14	  120	 0+120=120        
+#   2        4	   1      24    120+24=144
+#   3        1	   0	   1    144+1=145
+
