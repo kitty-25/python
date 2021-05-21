90033500cc0c6740ec33eceaa27310e147604fc2
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-21 23:26:55 +0930

    Add files via upload

---
+++ 21 may 2020/pattern33.py
@@ -0,0 +1,13 @@
+#E J O T Y 
+#D I N S X 
+#C H M R W 
+#B G L Q V 
+#A F K P U
+
+
+for i in reversed(range(65,70)):
+    res=i
+    for j in range(5):
+        print(chr(res),end=" ")
+        res+=5
+    print()
