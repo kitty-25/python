d29e81437c1f6757f385e969b089fec7a8ab03ff
kitty-25 <62871079+kitty-25@users.noreply.github.com>
2021-05-21 21:29:48 +0930

    Add files via upload

---
+++ 20 may 2021/pattern22.py
@@ -0,0 +1,11 @@
+#0 0 0 0 0
+#1 1 1 1 1
+#0 0 0 0 0
+#1 1 1 1 1
+#0 0 0 0 0
+
+for i in range(2,7):
+    for j in range(1,6):
+        
+        print(str(i%2),end=" ")
+    print()
---
+++ 20 may 2021/pattern23.py
@@ -0,0 +1,11 @@
+#1 1 1 1 1
+#0 0 0 0 0
+#1 1 1 1 1
+#0 0 0 0 0
+#1 1 1 1 1
+
+for i in range(1,6):
+    for j in range(1,6):
+        
+        print(str(i%2),end=" ")
+    print()
